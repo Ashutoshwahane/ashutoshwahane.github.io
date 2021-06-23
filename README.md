b5e97652f8fc8b3567b728297fac134ca58044ce
ashutoshwahane <ashutosh.wahane10@gmail.com>
2021-06-23 19:36:35 +0530

    Updates

--- README.md
+++
@@ -1,5 +0,0 @@
-
-Hi [ I'm Ashutosh Wahane](https://ashutoshwahane.github.io)
-
-
-<img float="left" height="370" src="images/desktop.png" alt="Desktop Preview" role="img" aria-label="desktop screenshot"> <img align="right" height="370" src="images/mobile.jpeg" alt="Mobile Preview" role="img" aria-label="mobile screenshot">
---
+++ precache-manifest.d5f8ab08c3f2fdac1d51f1f176bb88f3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "74b5a1de7fb88c275196ee9dda4e575e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "101baa955d95766c4ea7",
+    "url": "/static/css/main.b58d8b01.chunk.css"
+  },
+  {
+    "revision": "d6e2f4e05df6cbcb123e",
+    "url": "/static/js/2.d9a93f38.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/static/js/2.d9a93f38.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "101baa955d95766c4ea7",
+    "url": "/static/js/main.9b3974cb.chunk.js"
+  },
+  {
+    "revision": "cb3ccd3eb684c83ffcee",
+    "url": "/static/js/runtime-main.823c8cbe.js"
+  }
+]);
